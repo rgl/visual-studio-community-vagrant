daf1defb69ccfbae00c7c51be79acf2703d3d1eb
Rui Lopes <rgl@ruilopes.com>
2017-08-06 10:16:37 +0100

    migrate to visual studio preview

--- provision-vs.ps1
+++ provision-vs.ps1
@@ -4,10 +4,11 @@ choco install -y netfx-4.7-devpack
 # add support for building applications that target the .net 4.6.2 framework.
 choco install -y netfx-4.6.2-devpack
 
+# see https://www.visualstudio.com/vs/preview/
 # see https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio
 # see https://docs.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples
-$archiveUrl = 'https://aka.ms/vs/15/release/vs_Community.exe'
-$archiveHash = '72e5b420b6732a6b26ab17daea0de473d9f2a33f239633d6113a2bd6c4e656aa'
+$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/11021200/02c8226ecf13eeb8d1d833f2d04377f0/vs_Community.exe' # v15.3 preview.
+$archiveHash = '8508b49eff9122d112810974ddae1b25c0cec0b6c6be6890ff158b490aa55ee3'
 $archiveName = Split-Path $archiveUrl -Leaf
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host 'Downloading the Visual Studio Setup Bootstrapper...'
@@ -17,16 +18,24 @@ if ($archiveHash -ne $archiveActualHash) {
     throw "$archiveName downloaded from $archiveUrl to $archivePath has $archiveActualHash hash witch does not match the expected $archiveHash"
 }
 Write-Host 'Installing Visual Studio...'
-&$archivePath `
-    --installPath C:\VisualStudio2017Community `
-    --add Microsoft.VisualStudio.Workload.CoreEditor `
-    --add Microsoft.VisualStudio.Workload.NetCoreTools `
-    --add Microsoft.VisualStudio.Workload.NetWeb `
-    --add Microsoft.VisualStudio.Workload.ManagedDesktop `
-    --norestart `
-    --quiet `
-    --wait `
-    | Out-String -Stream
-if ($LASTEXITCODE) {
-    throw "Failed to install Visual Studio with Exit Code $LASTEXITCODE"
+for ($try = 1; ; ++$try) {
+    &$archivePath `
+        --installPath C:\VisualStudio2017PreviewCommunity `
+        --add Microsoft.VisualStudio.Workload.CoreEditor `
+        --add Microsoft.VisualStudio.Workload.NetCoreTools `
+        --add Microsoft.VisualStudio.Workload.NetWeb `
+        --add Microsoft.VisualStudio.Workload.ManagedDesktop `
+        --norestart `
+        --quiet `
+        --wait `
+        | Out-String -Stream
+    if ($LASTEXITCODE) {
+        if ($try -le 5) {
+            Write-Host "Failed to install Visual Studio with Exit Code $LASTEXITCODE. Trying again (hopefully the error was transient)..."
+            Start-Sleep -Seconds 10
+            continue
+        }
+        throw "Failed to install Visual Studio with Exit Code $LASTEXITCODE"
+    }
+    break
 }
