f70cd388c970ddede4c143f32bffaa5b975c9f53
Rui Lopes <rgl@ruilopes.com>
2019-06-21 21:30:50 +0100

    replace the deprecated vscode docker extension

--- provision.ps1
+++ provision.ps1
@@ -154,7 +154,7 @@ git config --global mergetool.meld.cmd '\"C:/Program Files (x86)/Meld/Meld.exe\"
     'ms-vscode.go'
     'ms-python.python'
     'mauve.terraform'
-    'peterjausovec.vscode-docker'
+    'ms-azuretools.vscode-docker'
     'zamerick.vscode-caddyfile-syntax'
 ) | ForEach-Object {
     code --install-extension $_
