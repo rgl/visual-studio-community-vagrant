08061c760bbf59f0ef59c37743a02cf73e2b9254
John Lazar <ionut.lazar@gmail.com>
2020-08-27 09:30:53 +0300

    fix Hyper-V install command, was missing a space

--- provision-hyper-v.ps1
+++ provision-hyper-v.ps1
@@ -1,5 +1,5 @@
 if (Get-Command -ErrorAction SilentlyContinue Install-WindowsFeature) {
-    Install-WindowsFeature Microsoft-Hyper-V-All
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 } else {
-    Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Hyper-V-All
+    Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Hyper-V -All
 }
