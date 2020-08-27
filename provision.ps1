8a7607626a19fbf496878648afed5347e9ec8361
Rui Lopes <rgl@ruilopes.com>
2020-08-27 07:36:50 +0100

    upgrade to chrome 85

--- provision.ps1
+++ provision.ps1
@@ -96,8 +96,8 @@ Start-Service Audiosrv
 # install Google Chrome.
 # see https://www.chromium.org/administrators/configuring-other-preferences
 choco install -y --ignore-checksums googlechrome
-$chromeLocation = 'C:\Program Files (x86)\Google\Chrome\Application'
-cp -Force GoogleChrome-external_extensions.json (Get-Item "$chromeLocation\*\default_apps\external_extensions.json").FullName
+$chromeLocation = 'C:\Program Files\Google\Chrome\Application'
+cp -Force GoogleChrome-external_extensions.json (Resolve-Path "$chromeLocation\*\default_apps\external_extensions.json")
 cp -Force GoogleChrome-master_preferences.json "$chromeLocation\master_preferences"
 cp -Force GoogleChrome-master_bookmarks.html "$chromeLocation\master_bookmarks.html"
 
