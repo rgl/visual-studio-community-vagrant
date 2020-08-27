e614504a349738b90b4a7df582769a6e3412f98c
Rui Lopes <rgl@ruilopes.com>
2020-08-27 22:46:12 +0100

    only install WSL2 in Windows 2004+

--- provision-wsl2.ps1
+++ provision-wsl2.ps1
@@ -1,6 +1,14 @@
 # see https://ubuntu.com/blog/ubuntu-on-wsl-2-is-generally-available
 # see https://aka.ms/wsl2kernel
 
+# only install WSL2 in Windows 2004+.
+$windowsCurrentVersionKey = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
+$windowsBuildNumber = $windowsCurrentVersionKey.CurrentBuildNumber
+if ($windowsBuildNumber -lt 19041) {
+    Write-Host "WARN: WSL2 was skipped because you need Windows Build 19041+ (aka Windows 10 2004) and you are using Windows Build $windowsBuildNumber."
+    Exit 0
+}
+
 Write-Host 'Downloading the WSL2 kernel...'
 $archiveUrl = 'https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi'
 $archiveName = Split-Path -Leaf $archiveUrl
