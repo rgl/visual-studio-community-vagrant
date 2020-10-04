e4d3e96331543bbb039db97b956de7c78bf6600d
Rui Lopes <rgl@ruilopes.com>
2020-10-04 09:39:55 +0100

    upgrade to vs 16.7.5

--- provision-vs.ps1
+++ provision-vs.ps1
@@ -7,15 +7,15 @@ choco install -y netfx-4.7.1-devpack
 # add support for building applications that target the .net 4.6.2 framework.
 choco install -y netfx-4.6.2-devpack
 
-# install Visual Studio Community 2019 16.7.3.
+# install Visual Studio Community 2019 16.7.5.
 # see https://www.visualstudio.com/vs/
 # see https://visualstudio.microsoft.com/downloads/
 # see https://docs.microsoft.com/en-us/visualstudio/releases/2019/release-notes
 # see https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio
 # see https://docs.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples
 # see https://docs.microsoft.com/en-us/visualstudio/install/workload-and-component-ids
-$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/44fbb1ed-c06e-41c9-bc39-3d7f2083d61b/57a346bc1f6a3d9018057aa275f7a9e9b78307fce28b220ba50ef3c2fbc4127f/vs_Community.exe'
-$archiveHash = '57a346bc1f6a3d9018057aa275f7a9e9b78307fce28b220ba50ef3c2fbc4127f'
+$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/e8bc3741-cb70-42aa-9b4e-2bd497de85dd/6b53cd77feaf149d8baca22797482e4af7f68964809e074a772eddf67f618fe5/vs_Community.exe'
+$archiveHash = '6b53cd77feaf149d8baca22797482e4af7f68964809e074a772eddf67f618fe5'
 $archiveName = Split-Path $archiveUrl -Leaf
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host 'Downloading the Visual Studio Setup Bootstrapper...'
