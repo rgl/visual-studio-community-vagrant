3d2a4a4f4d5ba77489505119ae4dd30fd283c005
Rui Lopes <rgl@ruilopes.com>
2021-03-13 09:58:43 +0000

    upgrade to docker 20.10.5

--- provision-docker-ce.ps1
+++ provision-docker-ce.ps1
@@ -1,13 +1,13 @@
 # see https://docs.microsoft.com/en-us/virtualization/windowscontainers/manage-docker/configure-docker-daemon
 # see https://docs.docker.com/engine/installation/linux/docker-ce/binaries/#install-server-and-client-binaries-on-windows
-# see https://github.com/moby/moby/releases/tag/v20.10.3
-# see https://github.com/rgl/docker-ce-windows-binaries-vagrant/releases/tag/v20.10.3
+# see https://github.com/moby/moby/releases/tag/v20.10.5
+# see https://github.com/rgl/docker-ce-windows-binaries-vagrant/releases/tag/v20.10.5
 
 # download install the docker binaries.
-$archiveVersion = '20.10.3'
+$archiveVersion = '20.10.5'
 $archiveName = "docker-$archiveVersion.zip"
 $archiveUrl = "https://github.com/rgl/docker-ce-windows-binaries-vagrant/releases/download/v$archiveVersion/$archiveName"
-$archiveHash = 'aeb6fbf3165f8cc0eda6e214d4aeaf8c03ea1cb48afa377b370409ab43d1cd88'
+$archiveHash = 'fe3efa2497fa71835fc37ce424fdf0481d25548251928c49b44942837d00a40f'
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host "Installing docker $archiveVersion..."
 (New-Object System.Net.WebClient).DownloadFile($archiveUrl, $archivePath)
