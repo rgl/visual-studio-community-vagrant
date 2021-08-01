6b3426a3085143292f775e041df00fb97cf975db
Rui Lopes <rgl@ruilopes.com>
2021-08-01 11:58:03 +0100

    retry the hyper-v installation until it succeeds

--- provision-hyper-v.ps1
+++ provision-hyper-v.ps1
@@ -1,5 +1,14 @@
-if (Get-Command -ErrorAction SilentlyContinue Install-WindowsFeature) {
-    Install-WindowsFeature Hyper-V -IncludeManagementTools
-} else {
-    Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Hyper-V-All
+while ($true) {
+    try {
+        if (Get-Command -ErrorAction SilentlyContinue Install-WindowsFeature) {
+            Install-WindowsFeature Hyper-V -IncludeManagementTools
+        } else {
+            Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Hyper-V-All
+        }
+        break
+    } catch {
+        Write-Host "WARN: Failed to install Hyper-V: $_"
+        Start-Sleep -Seconds 15
+        Write-Host "Retrying installation..."
+    }
 }
